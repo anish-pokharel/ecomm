2e65eb417bbf2e1f3378294fbc38b1f8b35d61e0
Anish Pokharel <anishpokharel@Anishs-MacBook-Air.local>
2025-05-22 23:09:58 +0545

    final pish

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MyAccountComponent } from './pages/my-account/my-account.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomepageComponent },
+    { path: 'cart', component: CartComponent },
+    { path: 'checkout', component: CheckoutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: LoginComponent },
+    { path: 'my-account', component: MyAccountComponent },
+    { path: 'product-details', component: ProductDetailsComponent },
+    { path: 'product-list', component: ProductListComponent },
+];
+
